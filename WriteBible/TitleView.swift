d07d45471b76de190646e47b325f6fd6402deed1
Leetaek <retake_joy@naver.com>
2022-06-09 12:58:10 +0900

    [fix] titleSubView 수정

--- WriteBible/TitleView.swift
+++ WriteBible/TitleView.swift
@@ -12,43 +12,70 @@ struct TitleView: View {
     @Binding var chapter: Int
     
     var body: some View {
-        let lastChapter = Bible(title: title.rawValue, chapterTitle: nil).lastChapter()
-        
+        VStack {
+            contents
+            
+            bibleList
+
+        }
+    }
+    
+    //MARK: - 제목
+    var contents: some View {
         HStack {
-        
-            Picker("성경본문", selection: $title) {
-                ForEach(BibleTitle.allCases, id: \.self) {
-                    Text($0.rawValue)
-                        .tag($0)
-                }
+            Text("목차")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            
+            Spacer()
+            
+            Button(action: {print("X")}) {
+                Image(systemName: "x.circle")
+                    .foregroundColor(.black)
+                    
             }
             .padding()
             
-            
-            Picker("절", selection: $chapter){
-                ForEach((1...lastChapter), id: \.self) {
-                    Text("\($0)")
-                        .tag($0)
-                }.listStyle(.insetGrouped)
-            }
-            
-            
-            
         }
         
-            
-
-    
     }
     
+    //MARK: - 성경 목차
+    var bibleList: some View {
+        let lastChapter = Bible(title: title.rawValue, chapterTitle: nil).lastChapter()
+        
+        return HStack {
+            List {
+                ForEach(BibleTitle.allCases, id: \.self) { value in
+                    let ti = value.rawValue.components(separatedBy: ".").first!
+                    let name = ti[4..<ti.count]
+                    
+                    Button(action: { self.title = value}) {
+                        Text("\(name)")
+                    }
+                }
+            }
+            .listStyle(.plain)
+            
+            List{
+                ForEach((1...lastChapter), id: \.self) { value in
+                    Button(action: { self.chapter = value}) {
+                        Text("\(value)")
+                    }
+                }
+            }.listStyle(.plain)
+        }
+    }
     
     
     
 }
 
-//struct TitleView_Previews: PreviewProvider {
-//   
-//    static var previews: some View {
-//        TitleView(title: "$bibleTitle", chapter: Binding(0..<3))
-//    }
-//}
+struct TitleView_Previews: PreviewProvider {
+   
+    
+    static var previews: some View {
+        TitleView(title: .constant(.genesis), chapter: .constant(1))
+    }
+}
