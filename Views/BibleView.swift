836796f332c4d37a9be505f1aa17395318d12e95
Leetaek <retake_joy@naver.com>
2022-06-24 13:08:23 +0900

    bibleView 전환시 스크롤 가장 위로

--- Views/BibleView.swift
+++ Views/BibleView.swift
@@ -25,39 +25,47 @@ struct BibleView: View {
         let keyTitle = bibleTitle.rawValue + chapterNum.description
       
         return GeometryReader { geo in
-            ScrollView(.vertical) {
-                    ForEach(bible.makeBible(title: bibleTitle.rawValue).filter{$0.chapter == chapterNum}, id: \.sentence ) { name in
-                            HStack (alignment: .top){
-                                // 성경 구절
-                                BibleSentenceView(bibleSentence: name)
-                                    .frame(width: geo.size.width/2, alignment: .leading)
+            ScrollViewReader { scr in
+                ScrollView(.vertical) {
+                        ForEach(bible.makeBible(title: bibleTitle.rawValue).filter{$0.chapter == chapterNum}, id: \.sentence ) { name in
+                                HStack (alignment: .top){
+                                    // 성경 구절
+                                    BibleSentenceView(bibleSentence: name)
+                                        .frame(width: geo.size.width/2, alignment: .leading)
 
+                                        
+                                    // 필사 뷰 절 번호
+                                    Text("\(name.section)")
+                                        .bold()
+                                        .font(.system(size: 17))
+                                        
                                     
-                                // 필사 뷰 절 번호
-                                Text("\(name.section)")
-                                    .bold()
-                                    .font(.system(size: 17))
-                                    
-                                
-                                // 필사하는 부분 line
-                                VStack (alignment: .leading, spacing: 30){
-                                    ForEach(1..<5, id: \.self) { _ in
-                                        Rectangle()
-                                          .opacity(0.2)
-                                          .frame(width: geo.frame(in: .global).size.width/7 * 3, height: 2)
-                                          .position(x: (geo.frame(in: .global).midX + geo.frame(in: .global).minX)/2 - 20 , y: 22)
-                                    }
-                                }/// LazyVStack
-                            .padding([.bottom])
-                            .listRowSeparator(.hidden)
-                    } /// ForEach
-                }
-                    .overlay() {
-                        DrawingWrapper(manager: manager, title: keyTitle)
-                            .id(keyTitle)
-                    }
-                    .listStyle(.plain)
-                } /// scrollView
+                                    // 필사하는 부분 line
+                                    VStack (alignment: .leading, spacing: 30){
+                                        ForEach(1..<5, id: \.self) { _ in
+                                            Rectangle()
+                                              .opacity(0.2)
+                                              .frame(width: geo.frame(in: .global).size.width/7 * 3, height: 2)
+                                              .position(x: (geo.frame(in: .global).midX + geo.frame(in: .global).minX)/2 - 20 , y: 22)
+                                        }
+                                    }/// VStack
+                                .padding([.bottom])
+                                .listRowSeparator(.hidden)
+                            }/// HStack
+                        }/// ForEach
+                        .id("scrollToTop")
+                        .overlay() {
+                            DrawingWrapper(manager: manager, title: keyTitle)
+                                .id(keyTitle)
+                        }
+                        .listStyle(.plain)
+                        .onChange(of: keyTitle) { newValue in
+                            withAnimation(.default) {
+                                scr.scrollTo("scrollToTop", anchor: .top)
+                            }
+                        }
+                    } /// scrollView
+                }/// scrollViewReader
             } /// geometry
         }
     
